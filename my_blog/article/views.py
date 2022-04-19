7e7feab42a777aa9d02b8711274d81a6560b695c
gs <gs2018211242@bupt.edu.cn>
2022-04-19 20:36:01 +0800

    add markdown

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
 
 
 def article_list(request):
@@ -11,6 +12,12 @@ def article_list(request):
 
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
+
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         'markdown.extensions.extra',
+                                         'markdown.extensions.codehilite',
+                                     ])
+
     context = {'article': article}
     return render(request, 'article/detail.html', context)
-
