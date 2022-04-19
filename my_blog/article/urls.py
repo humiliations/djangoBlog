07874158e74d9fc116bcfb6baf82f3d7919dbe83
gs <gs2018211242@bupt.edu.cn>
2022-04-19 19:45:45 +0800

    4.19 night

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -8,4 +8,5 @@ app_name = 'article'
 urlpatterns = [
     # urls
     path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
