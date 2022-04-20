080296fedf0b7fafb14579e726fc85154b22cb04
gs <gs2018211242@bupt.edu.cn>
2022-04-20 11:48:37 +0800

    4.20 noon

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # urls
     path('article-list/', views.article_list, name='article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
+    path('artilce-safe-delete/<int:id>/', views.article_safe_delete, name='article_safe_delete'),
 ]
